8178187290975a9f8202b3cf58f7c4b21a35c6c0
Berat Dilki <bd154494@renishaw.com>
2024-09-06 12:27:17 +0100

    remove unnecessary using statements

--- weather-app/Utilities/WeatherService.cs
+++ weather-app/Utilities/WeatherService.cs
@@ -1,10 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 using weather_app.Weather;
 using weather_app.Weather.Units;
 
